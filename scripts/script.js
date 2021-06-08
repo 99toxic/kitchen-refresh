8f0392daf140d9fd74e496e85f92b925e5246761
99toxic <nsimpson1983@gmail.com>
2021-06-08 09:42:19 -0400

    worked more on css got the elements selected

--- scripts/script.js
+++ scripts/script.js
@@ -4,12 +4,32 @@ const pageOne = document.querySelector('.page__one');
 const pageTwo = document.querySelector('.page__two');
 const pageThree = document.querySelector('.page__three');
 
+const formDoors = document.getElementById('doors');
+const formGlass = document.getElementById('glass');
+const formDrawers = document.getElementById('drawers');
+const formWall = document.getElementById('wall');
+const formTray = document.getElementById('tray');
+
+const formName = document.getElementById('name');
+const formCity = document.getElementById('city');
+const formState = document.getElementById('state');
+const formEmail = document.getElementById('email');
+const formTel = document.getElementById('tel');
+const formCheckbox = document.getElementById('checkbox');
+
+const submitBtn = document.querySelector('.container__button__submit');
+
+// When 'next' button is clicked it goes to next page
 const nextPage = function () {
   document.querySelectorAll(`.container__button__next`).forEach(item => {
     item.addEventListener('click', () => {
       const pageName = item.parentElement.parentElement.className;
 
       if (pageName === 'page page__one') {
+        if (!$('.container__box__img').hasClass('picked')) {
+          alert('Please Select Your Finish');
+          return;
+        }
         pageTwo.classList.remove('hidden');
         pageOne.classList.add('hidden');
       }
@@ -22,6 +42,7 @@ const nextPage = function () {
   });
 }; //end nextPage fn
 
+// When 'previous' button is clicked it goes back a page
 const previousPage = function () {
   document.querySelectorAll('.container__button__previous').forEach(item => {
     item.addEventListener('click', () => {
@@ -40,10 +61,7 @@ const previousPage = function () {
   });
 }; //end previousPage fn
 
-const removeItem = function () {
-  removeEventListener('click', removeItem);
-}; //end removeItem fn
-
+// Selects item when clicked with class of picked
 const selectItems = function (items) {
   $('div[data-selection] div').click(function () {
     if ($(this).parent('div').attr('data-selection') == items) {
@@ -52,9 +70,32 @@ const selectItems = function (items) {
     } else if ($(this).parent('div').attr('data-selection') == 'multiple') {
       $(this).find('img').toggleClass('picked');
     }
-    console.log(document.querySelectorAll('.picked'));
   });
 }; //end selectItems fn
+
+// When form submits
+submitBtn.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const itemsPicked = document.querySelectorAll('.picked');
+
+  console.log(
+    'test 1',
+    itemsPicked,
+    formDoors.value,
+    'test 2',
+    formGlass.value,
+    formDrawers.value,
+    formWall.value,
+    formTray.value,
+    formName.value,
+    formCity.value,
+    formState.value,
+    formEmail.value,
+    formTel.value,
+    formCheckbox.checked
+  );
+});
 
 nextPage();
 previousPage();
